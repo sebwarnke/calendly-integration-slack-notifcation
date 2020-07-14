c0bbcb2a01ec7fdabd0c9ae76cbba40973312cec
Sebastian Warnke <sw-github@sebwarnke.com>
2020-07-14 14:16:12 +0200

    achieve proof of concept

--- src/main/java/com/sebwarnke/calendlyintegration/delegates/NotifySlackDelegate.java
+++ src/main/java/com/sebwarnke/calendlyintegration/delegates/NotifySlackDelegate.java
@@ -1,5 +1,6 @@
 package com.sebwarnke.calendlyintegration.delegates;
 
+import com.sebwarnke.calendlyintegration.model.CalendlyEvent;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
@@ -10,38 +11,40 @@ import java.net.URL;
 
 @Component
 public class NotifySlackDelegate implements JavaDelegate {
-    @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        String event = (String) execution.getVariable("event");
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    CalendlyEvent calendlyEvent = (CalendlyEvent) execution.getVariable("calendlyEvent");
 
-        final String POST_BODY ="{\n" + "\"userId\": 101,\r\n" +
-                "    \"id\": 101,\r\n" +
-                "    \"title\": \"Test Title\",\r\n" +
-                "    \"body\": \"Test Body\"" + "\n}";
+    String incomingEvent = calendlyEvent.getEvent();
+    String eventName = calendlyEvent.getPayload().getEventType().getName();
+    String inviteeName = calendlyEvent.getPayload().getInvitee().getName();
+    String startTime = calendlyEvent.getPayload().getEvent().getStartTimePretty();
+    String endTime = calendlyEvent.getPayload().getEvent().getEndTimePretty();
 
-        URL url = new URL ("slackwebhook.com");
-        HttpURLConnection slackCon = (HttpURLConnection) url.openConnection();
-        slackCon.setDoOutput(true);
-        slackCon.setRequestMethod("Post");
-        slackCon.setRequestProperty("Content-Type", "application/json");
+    String slackMessage = incomingEvent.equals("invitee.created") ?
+      "The event \\\"" + eventName + "\\\" was just scheduled by " + inviteeName + ". Time: " + startTime +
+        " - " + endTime : "The event \\\"" + eventName + "\\\" originally scheduled by " + inviteeName + " was just " +
+      "canceled. Time: " + startTime +
+      " - " + endTime;
 
+  final String POST_BODY = "{\"text\" : \"" + slackMessage + "\"}";
 
-
-        OutputStream os = slackCon.getOutputStream();
-        os.write(POST_BODY.getBytes());
-        os.flush();
-        os.close();
-
-        int responseCode = slackCon.getResponseCode();
-        String responseMessage = slackCon.getResponseMessage();
-        System.out.print("Response Code: "+ responseCode);
-        System.out.print("Response message: " + responseMessage);
+  URL url = new URL("https://hooks.slack.com/services/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");
+  HttpURLConnection slackCon = (HttpURLConnection) url.openConnection();
+    slackCon.setDoOutput(true);
+    slackCon.setRequestMethod("POST");
+    slackCon.setRequestProperty("Content-Type","application/json");
 
 
+  OutputStream os = slackCon.getOutputStream();
+    os.write(POST_BODY.getBytes());
+    os.flush();
+    os.close();
 
+  int responseCode = slackCon.getResponseCode();
+  String responseMessage = slackCon.getResponseMessage();
+    System.out.print("Response Code: "+responseCode);
+    System.out.print("Response message: "+responseMessage);
 
-
-
-
-    }
+}
 }
--- src/main/java/com/sebwarnke/calendlyintegration/model/Event.java
+++ src/main/java/com/sebwarnke/calendlyintegration/model/Event.java
@@ -11,7 +11,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "uuid",
