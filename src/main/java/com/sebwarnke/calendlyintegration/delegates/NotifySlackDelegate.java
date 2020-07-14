7b76542a97384fa3654a3a1922521ae2b3e872dd
nlea <nuhlemann@uni-potsdam.de>
2020-07-14 11:01:36 +0200

    adding the Delegate

--- src/main/java/com/sebwarnke/calendlyintegration/delegates/NotifySlackDelegate.java
+++ src/main/java/com/sebwarnke/calendlyintegration/delegates/NotifySlackDelegate.java
@@ -14,17 +14,28 @@ public class NotifySlackDelegate implements JavaDelegate {
     public void execute(DelegateExecution execution) throws Exception {
         String event = (String) execution.getVariable("event");
 
+        final String POST_BODY ="{\n" + "\"userId\": 101,\r\n" +
+                "    \"id\": 101,\r\n" +
+                "    \"title\": \"Test Title\",\r\n" +
+                "    \"body\": \"Test Body\"" + "\n}";
+
         URL url = new URL ("slackwebhook.com");
         HttpURLConnection slackCon = (HttpURLConnection) url.openConnection();
         slackCon.setDoOutput(true);
         slackCon.setRequestMethod("Post");
         slackCon.setRequestProperty("Content-Type", "application/json");
 
-        String input = "{\"text\": event}";
+
 
         OutputStream os = slackCon.getOutputStream();
-        os.write(input.getBytes());
+        os.write(POST_BODY.getBytes());
         os.flush();
+        os.close();
+
+        int responseCode = slackCon.getResponseCode();
+        String responseMessage = slackCon.getResponseMessage();
+        System.out.print("Response Code: "+ responseCode);
+        System.out.print("Response message: " + responseMessage);
 
 
 
