ec100ce2614b4b2e233929b476507a574463515a
Sebastian <sebastian.warnke@camunda.com>
2020-08-13 15:23:16 +0200

    implement logging

--- src/main/java/com/sebwarnke/calendlyintegration/api/CalendlyWebhookController.java
+++ src/main/java/com/sebwarnke/calendlyintegration/api/CalendlyWebhookController.java
@@ -1,14 +1,9 @@
 package com.sebwarnke.calendlyintegration.api;
 
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sebwarnke.calendlyintegration.config.LoggerConfiguration;
 import com.sebwarnke.calendlyintegration.model.CalendlyEvent;
 import com.sebwarnke.calendlyintegration.services.OrchestratorService;
-import com.sebwarnke.calendlyintegration.services.SlackNotificationService;
-import com.sebwarnke.calendlyintegration.util.Util;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,30 +17,23 @@ import java.io.IOException;
 @RequestMapping("/calendly")
 public class CalendlyWebhookController {
 
-  Logger logger = (Logger) LoggerFactory.getLogger("Events");
+    Logger logger = LoggerFactory.getLogger(CalendlyWebhookController.class);
 
 
-  private OrchestratorService orchestratorService;
+    private OrchestratorService orchestratorService;
 
-   @Autowired
-  public CalendlyWebhookController(OrchestratorService orchestratorService, LoggerConfiguration loggerConfiguration) {
-    this.orchestratorService = orchestratorService;
-     if(((LoggerContext) LoggerFactory.getILoggerFactory()) == null){
-       this.logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("Events");
-     } else {
-       Util.createLoggerFor("Events", loggerConfiguration.getOutput());
-     }
+    @Autowired
+    public CalendlyWebhookController(OrchestratorService orchestratorService) {
+        this.orchestratorService = orchestratorService;
+    }
 
-  }
+    @PostMapping("/deliver")
+    public void onEventReceived(@RequestBody CalendlyEvent calendlyEvent) throws IOException {
 
-  @PostMapping("/deliver")
-  public void onEventReceived(@RequestBody CalendlyEvent calendlyEvent) throws IOException {
-
-    logger.info("New even created: " + new ObjectMapper().writeValueAsString(calendlyEvent));
+        logger.info("New even created: " + new ObjectMapper().writeValueAsString(calendlyEvent));
 
 
-
-    orchestratorService.processCalendlyEvent(calendlyEvent);
-  }
+        orchestratorService.processCalendlyEvent(calendlyEvent);
+    }
 
 }
