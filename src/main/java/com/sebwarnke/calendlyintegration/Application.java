651764984703b4a1af556c3140acbd681a713f0b
Sebastian <sebastian.warnke@camunda.com>
2020-08-14 17:04:11 +0200

    improve formatting

--- src/main/java/com/sebwarnke/calendlyintegration/Application.java
+++ src/main/java/com/sebwarnke/calendlyintegration/Application.java
@@ -1,19 +1,14 @@
 package com.sebwarnke.calendlyintegration;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-
-
   }
 
 }
